946500ec4b400f372afa4399fb788b4c294ed2e4
rui <yejiurui@126.com>
2016-11-20 22:23:46 +0800

    个人页面增加员工编号选项

--- src/com/meijialife/dingdang/activity/PersonInfoActivity.java
+++ src/com/meijialife/dingdang/activity/PersonInfoActivity.java
@@ -9,19 +9,16 @@ import com.meijialife.dingdang.BaseActivity;
 import com.meijialife.dingdang.R;
 import com.meijialife.dingdang.bean.UserIndexData;
 
-
 /**
  * 个人信息
+ * 
  * @author windows7
- *
+ * 
  */
- 
-public class PersonInfoActivity extends BaseActivity   {
 
- 
-	private UserIndexData userIndexData;
+public class PersonInfoActivity extends BaseActivity {
 
-    
+    private UserIndexData userIndexData;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,30 +33,30 @@ public class PersonInfoActivity extends BaseActivity   {
         setTitleName("个人信息");
         requestBackBtn();
         userIndexData = (UserIndexData) getIntent().getSerializableExtra("userIndexData");
-        
+
         TextView tv_mobile = (TextView) findViewById(R.id.tv_mobile);
         TextView tv_sex = (TextView) findViewById(R.id.tv_sex);
         TextView tv_name = (TextView) findViewById(R.id.tv_name);
         TextView tv_jineng = (TextView) findViewById(R.id.tv_jineng);
-        
-        if(null!=userIndexData){
-            String skillString="";
+        TextView tv_staff_code = (TextView) findViewById(R.id.tv_staff_code);
+
+        if (null != userIndexData) {
+            String skillString = "";
             ArrayList<String> skills = userIndexData.getSkills();
             for (String str : skills) {
-                skillString += str +"\n"+"\n";
+                skillString += str + "\n" + "\n";
             }
-            tv_mobile.setText(userIndexData.getMobile());
-            tv_name.setText(userIndexData.getName());
-            tv_jineng.setText(skillString);
+            tv_mobile.setText(userIndexData.getMobile() == null ? " " : userIndexData.getMobile());
+            tv_name.setText(userIndexData.getName() == null ? " " : userIndexData.getName());
+//            tv_jineng.setText(skillString == null ? " " : skillString);
             int sex = userIndexData.getSex();
-            if(sex==0){
+            if (sex == 0) {
                 tv_sex.setText("男");
-            }else if(sex==1){
+            } else if (sex == 1) {
                 tv_sex.setText("女");
             }
+
+            tv_staff_code.setText(userIndexData.getStaff_code() == null ? " " : userIndexData.getStaff_code());
         }
     }
-
-   
-
 }
--- src/com/meijialife/dingdang/bean/UserIndexData.java
+++ src/com/meijialife/dingdang/bean/UserIndexData.java
@@ -5,88 +5,121 @@ import java.util.ArrayList;
 
 /**
  * 用户数据（个人主页接口）
- *
+ * 
  */
 public class UserIndexData implements Serializable {
+
+    private int staff_id;//
+    private int staff_type;// 员工类型 0 = 全职 1 = 兼职
+    private int status;// 员工状态 0 = 不可用 1 = 可用, 员工总开关
+    private String name;//
+    private int sex;//
+    private String mobile;//
+    private String head_img;
+    private int auth_status; // 身份认证 0 = 未认证 1 = 认证
+    private int total_order; // 本月订单
+    private double total_incoming;// 本月收入
+    private ArrayList<String> skills;// 技能
+    private String staff_code;// 员工编号
+
     
-	private int staff_id;//
-	private int staff_type;//员工类型 0 = 全职 1 = 兼职
-	private int status;//员工状态 0 = 不可用 1 = 可用, 员工总开关
-	private String name;//
-	private int sex;//
-	private String mobile;//
-	private String head_img; 
-	private int auth_status; //身份认证 0 = 未认证 1 = 认证
-	private int total_order; //本月订单
-	private double total_incoming;// 本月收入
-	private ArrayList<String> skills;//技能
+    
+    public String getStaff_code() {
+        return staff_code;
+    }
+
+    public void setStaff_code(String staff_code) {
+        this.staff_code = staff_code;
+    }
+
     public int getStaff_id() {
         return staff_id;
     }
+
     public void setStaff_id(int staff_id) {
         this.staff_id = staff_id;
     }
+
     public int getStaff_type() {
         return staff_type;
     }
+
     public void setStaff_type(int staff_type) {
         this.staff_type = staff_type;
     }
+
     public int getStatus() {
         return status;
     }
+
     public void setStatus(int status) {
         this.status = status;
     }
+
     public String getName() {
         return name;
     }
+
     public void setName(String name) {
         this.name = name;
     }
+
     public int getSex() {
         return sex;
     }
+
     public void setSex(int sex) {
         this.sex = sex;
     }
+
     public String getMobile() {
         return mobile;
     }
+
     public void setMobile(String mobile) {
         this.mobile = mobile;
     }
+
     public String getHead_img() {
         return head_img;
     }
+
     public void setHead_img(String head_img) {
         this.head_img = head_img;
     }
+
     public int getAuth_status() {
         return auth_status;
     }
+
     public void setAuth_status(int auth_status) {
         this.auth_status = auth_status;
     }
+
     public int getTotal_order() {
         return total_order;
     }
+
     public void setTotal_order(int total_order) {
         this.total_order = total_order;
     }
-   
+
     public double getTotal_incoming() {
-		return total_incoming;
-	}
-	public void setTotal_incoming(double total_incoming) {
-		this.total_incoming = total_incoming;
-	}
+        return total_incoming;
+    }
+
+    public void setTotal_incoming(double total_incoming) {
+        this.total_incoming = total_incoming;
+    }
+
     public ArrayList<String> getSkills() {
         return skills;
     }
+
     public void setSkills(ArrayList<String> skills) {
         this.skills = skills;
     }
+
     @Override
     public String toString() {
         return "UserIndexData [staff_id=" + staff_id + ", staff_type=" + staff_type + ", status=" + status + ", name=" + name + ", sex=" + sex
